db784fcaa26d1a0423c2d0c140692e14635132cf
Fernando Valani Cavalcante <valanife@gmail.com>
2021-05-10 21:38:09 -0300

    Implement Header

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Catalog from "./Catalog";
+import Session from "./Session";
+import Seat from "./Seat";
+import ConfirmPage from "./ConfirmPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Catalog />
+        </Route>
+        <Route path="/sessoes" exact>
+          <Session />
+        </Route>
+        <Route path="/assentos" exact>
+          <Seat />
+        </Route>
+        <Route path="/sucesso" exact>
+          <ConfirmPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
